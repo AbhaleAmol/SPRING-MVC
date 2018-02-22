dff6a0348d7f861ba7aaff82c7639e6c7055308c
AbhaleAmol <abhaleamol@hotmail.com>
2018-02-22 21:08:34 +0530

    Adding Archive Directory

---
+++ Archive/SampleSpringMVCWithGRADLEVer1.0/src/main/java/com/sforce/spring/mvc/controller/ShowHomePageController.java
@@ -0,0 +1,22 @@
+package com.sforce.spring.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ShowHomePageController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		ModelAndView modelAndView = null;
+		modelAndView = new ModelAndView();
+		
+		modelAndView.setViewName("home");
+		
+		return modelAndView;
+	}
+
+}
---
+++ Archive/SpringSimpleURLHandlerMappingGRADLE/src/main/java/com/spring/mvc/controller/ShowHomePageController.java
@@ -0,0 +1,25 @@
+package com.spring.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ShowHomePageController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		System.out.println("In Controller");
+		
+		ModelAndView modelAndView = null;
+		
+		modelAndView = new ModelAndView();
+		
+		modelAndView.setViewName("index");
+		
+		return modelAndView;
+	}
+
+}
